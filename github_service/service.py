a77ec946d838dc3d15dce973774fcad42168b765
Alex <lepsalex@gmail.com>
2019-05-02 19:45:01 -0400

    Trying things ...

--- github_service/service.py
+++ github_service/service.py
@@ -5,14 +5,14 @@ print("Github Service start!")
 
 url = os.getenv('SERVICE_URL', 'pulsar://localhost:6650')
 client = pulsar.Client(url)
-consumer = client.subscribe(['task-new', 'task-update'], 'github-service')
+consumer = client.subscribe("task-new", "github-service")
 
 print("Github Service running ...")
 
 while True:
-    msg = consumer.receive()
     try:
         print("Listening on topics: {}".format(consumer.topic()))
+        msg = consumer.receive()
         print("Received message '{}' id='{}'".format(msg.data(), msg.message_id()))
         # Acknowledge successful processing of the message
         consumer.acknowledge(msg)
